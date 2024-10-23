d92dc233eafb7ff612c8d08d7aef70625b2850e6
Ricky Javaroni <ricky@javaroni.com>
2024-10-23 15:45:09 -0700

    Initial Commit

---
+++ halloween/halloweenApp.swift
@@ -0,0 +1,17 @@
+//
+//  halloweenApp.swift
+//  halloween
+//
+//  Created by Ricky Javaroni on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct halloweenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ halloweenTests/halloweenTests.swift
@@ -0,0 +1,17 @@
+//
+//  halloweenTests.swift
+//  halloweenTests
+//
+//  Created by Ricky Javaroni on 10/23/24.
+//
+
+import Testing
+@testable import halloween
+
+struct halloweenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
